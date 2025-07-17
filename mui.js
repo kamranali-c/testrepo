bad9e2676eaaa4e9ea6ad377248b229b1dd9a5ed
kamranali-c <kamran.ali@capgemini.com>
2025-07-17 23:14:56 +0100

    Update mui.js

--- mui.js
+++ mui.js
@@ -1,10 +1,14 @@
+import { render, screen, within } from '@testing-library/react';
+import TaskTable from './TaskTable';
 
-  containers.forEach((container) => {
-    const utils = within(container);
-    const input = utils.queryByLabelText('Username');
-    if (input) {
-      inputNode = input;
-    }
-  });
+test('finds "Job ID" in one of the containers', () => {
+  render(<TaskTable />);
 
-  expect(inputNode).toBeInTheDocument();
+  const containers = screen.getAllByRole('presentation');
+
+  const found = containers.some((container) =>
+    within(container).queryByText('Job ID')
+  );
+
+  expect(found).toBe(true);
+});
